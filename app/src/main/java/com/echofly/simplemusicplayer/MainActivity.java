2ec3a743edcfde73298d73def1a4004073fbe6eb
Dave Davis <dave@redflymarketing.com>
2018-03-14 05:05:44 +0000

    Initial commit

---
+++ app/src/main/java/com/echofly/simplemusicplayer/MainActivity.java
@@ -0,0 +1,52 @@
+package com.echofly.simplemusicplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Grab a reference to the buttons in the activity_main.xml layout
+        Button playButton = findViewById(R.id.play);
+        Button pauseButton = findViewById(R.id.pause);
+
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.song);
+
+
+
+
+        // Set an OnClickListener for the play button
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //use the MediaPlayer to play the local song.
+                mediaPlayer.start();
+
+            }
+        });
+
+
+        // Set an OnClickListener for the play button
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Pause Song
+                mediaPlayer.pause();
+            }
+        });
+
+
+
+
+    }
+
+
+}
